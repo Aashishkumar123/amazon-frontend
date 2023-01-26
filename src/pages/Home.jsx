66046772005be3ecf5209d719c0c9198b477cafd
Aashish <aashishkumar12376@gmail.com>
2023-01-26 19:27:02 +0530

    [ADDED] banner section.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import { Navbar } from "../components/navbar/Navbar"
+import banner from "../static/images/71kv6y8hsyL._SX3000_.jpg"
 
 
 export const Home = ()=>{
     return(
         <>
             <Navbar />
+            <section className="amazon-banner">
+                <img src={banner} alt="amz-banner-img" className="img-fluid" />
+            </section>
         </>
     )
 }
